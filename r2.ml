973019dfdda8b49232e2d20898b06f8d803b6181
Qiyu Li <0x9357@gmail.com>
2017-01-11 00:32:08 +0800

    add let var bind

--- r2.ml
+++ r2.ml
@@ -6,15 +6,32 @@ let lexer = make_lexer ["+";"-";"*";"/";"(";")";"[";"]";"let"]
 let replace input output =
     Str.global_replace (Str.regexp_string input) output
 
-let rec interp = parser 
-    [< 'Int a >] -> a
-  | [< 'Kwd "("; e = interp; 'Kwd ")" >] -> e
-  | [< 'Kwd "+"; e1 = interp; e2 = interp >] -> e1 + e2
-  | [< 'Kwd "-"; e1 = interp; e2 = interp >] -> e1 - e2
-  | [< 'Kwd "*"; e1 = interp; e2 = interp >] -> e1 * e2
-  | [< 'Kwd "/"; e1 = interp; e2 = interp >] -> e1 / e2;;
+let env0 = []
+
+let lookup k env =
+  if List.mem_assoc k env
+  then Some (List.assoc k env)
+  else None
+
+let extEnv k v env = (k, v)::env 
+
+let tryGet v = 
+  match v with 
+  | Some x -> x
+  | None -> 42 
+
+let rec interp env = parser 
+    [< 'Int a >] -> Some a
+  | [< 'Ident a >] -> lookup a env 
+  | [< 'Kwd "("; e = interp env; 'Kwd ")" >] -> e
+  | [< 'Kwd "+"; e1 = interp env; e2 = interp env >] -> Some ((tryGet e1) + (tryGet e2))
+  | [< 'Kwd "-"; e1 = interp env; e2 = interp env >] -> Some ((tryGet e1) - (tryGet e2))
+  | [< 'Kwd "*"; e1 = interp env; e2 = interp env >] -> Some ((tryGet e1) * (tryGet e2))
+  | [< 'Kwd "/"; e1 = interp env; e2 = interp env >] -> Some ((tryGet e1) / (tryGet e2))
+  | [< 'Kwd "let"; 'Kwd "["; 'Ident a; e1 = interp env; 'Kwd "]"; e2 = interp (extEnv a (tryGet e1) env) >]
+             -> e2;;
 
 (*let () =
   let expr = Sys.argv.(1) in
-  printf "%d\n" (interp (lexer (Stream.of_string expr)));
+  printf "%d\n" (interp env (lexer (Stream.of_string expr)));
 ;;*)
--- test.ml
+++ test.ml
@@ -1,12 +1,14 @@
-open OUnit2;;
-let f s = s |> (R2.replace "(*" "( *") |> Stream.of_string |> R2.lexer |> R2.interp;;
+open OUnit2
 
-let addTest test_ctxt = assert_equal 3 (f "(+ 1 2)");;
-let minusTest test_ctxt = assert_equal 0 (f "(- 2 2)");;
-let multiTest test_ctxt = assert_equal 6 (f "(* 2 3)");;
-let diviTest test_ctxt = assert_equal 1 (f "(/ 2 2)");;
-let mixTest test_ctxt = assert_equal 1 (f "(/ (+ (* (- 2 3) (* 2 1)) 4) (+ 1 1))");;
+let f s = s |> (R2.replace "(*" "( *") |> Stream.of_string |> R2.lexer |> (R2.interp R2.env0) |> R2.tryGet
 
+let addTest test_ctxt = assert_equal 3 (f "(+ 1 2)")
+let minusTest test_ctxt = assert_equal 0 (f "(- 2 2)")
+let multiTest test_ctxt = assert_equal 6 (f "(* 2 3)")
+let diviTest test_ctxt = assert_equal 1 (f "(/ 2 2)")
+let mixTest test_ctxt = assert_equal 1 (f "(/ (+ (* (- 2 3) (* 2 1)) 4) (+ 1 1))")
+
+let letbindTest test_ctxt = assert_equal 6 (f "(let [x (+ 1 2)] (+ x 3))")
 
 let suite =
 "suite">:::
@@ -14,5 +16,6 @@ let suite =
   "minus test">:: minusTest;
   "multiply test">:: multiTest;
   "divide test">:: diviTest;
-  "mix calculate test">:: mixTest]
+  "mix calculate test">:: mixTest;
+  "let bind Test">:: letbindTest]
 ;;
